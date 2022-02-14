3ba2f8e9742956b9013217a295d873a862c482f4
leeapril0612 <leeapril0612@openobject.net>
2022-02-14 17:33:33 +0900

    Fix a main.ts use vue-cookies-ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import Vue from 'vue'
+import VueCookies from 'vue-cookies-ts'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+Vue.use(VueCookies)
+Vue.cookies.config({
+  expires: 60 * 5
+})
 
 new Vue({
   router,
