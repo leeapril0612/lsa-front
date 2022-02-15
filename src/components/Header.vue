446f694988c904ba32895d51b813caa98ac12430
leeapril0612 <leeapril0612@openobject.net>
2022-02-15 17:55:24 +0900

    Add Header components

---
+++ src/components/Header.vue
@@ -0,0 +1,75 @@
+<template>
+  <header v-bind:class="{
+    main: headerClass
+  }">
+    <div class="logo">
+      WeB
+    </div>
+    <div class="nav">
+      <router-link to="/" active-class="active" exact>Home</router-link> |
+      <router-link to="/about" active-class="active">Board</router-link> |
+      <router-link to="/login" active-class="active">Login</router-link>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+type Data = {
+  navItem: string[]
+}
+export default Vue.extend({
+  name: 'header',
+  data: function (): Data {
+    return {
+      navItem: []
+    }
+  },
+  computed: {
+    headerClass: function (): boolean {
+      return !this.$route.meta?.trn
+    }
+  },
+  mounted: function (): void {
+    console.log(this.$route)
+  },
+  watch: {
+    $route: function () {
+      console.log(this.$route)
+    }
+  }
+})
+</script>
+
+<style>
+  header {
+    position: absolute;
+    z-index: 3;
+    padding: 0 40px;
+    color: #fff;
+    width: 100%;
+    height: 60px;
+    display: flex;
+    justify-content: space-between;
+    line-height: 60px;
+    font-size: 14px;
+  }
+  header.main {
+    background: #000;
+  }
+  header .logo {
+    font-size: 1.3em;
+    font-weight: bold;
+  }
+
+  header .nav a {
+    font-weight: bold;
+    text-decoration: none;
+    color: #fff;
+  }
+
+  header .nav a.active {
+    color: #fbc22b;
+  }
+</style>
