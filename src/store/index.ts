5c86e970d0c08a8f20068add2858a5ed6f9869e0
leeapril0612 <leeapril0612@openobject.net>
2022-02-18 13:39:52 +0900

    Update store login

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,63 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getProfile, login } from '@/service/user/userAPI'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+type UserInfo = {
+  username: string,
+}
+
+type State = {
+  profile: UserInfo | null,
+  login: boolean
+}
+
+export default new Vuex.Store<State>({
   state: {
+    login: localStorage.getItem('login') === 'true',
+    profile: null
   },
   mutations: {
+    LOGIN (state) {
+      state.login = true
+    },
+    LOGOUT (state) {
+      state.login = false
+    },
+    setProfile (state, profile: UserInfo) {
+      console.log('setprofile')
+      state.profile = profile
+    }
   },
   actions: {
+    LOGIN ({ commit }, { username, password }) {
+      return new Promise((resolve, reject) => {
+        login({
+          username,
+          password
+        })
+          .then(() => {
+            commit('LOGIN')
+            this.dispatch('getUserInfo')
+              .then((res) => resolve(res))
+              .catch((err) => reject(err))
+          })
+          .catch((err) => reject(err))
+      })
+    },
+    LOGOUT ({ commit }) {
+      commit('LOGOUT')
+    },
+    getUserInfo ({ commit }) {
+      return getProfile()
+        .then((res) => {
+          commit('setProfile', res.data)
+          return res
+        })
+    }
+  },
+  getters: {
   },
   modules: {
   }
