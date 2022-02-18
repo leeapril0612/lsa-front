b9022e45d9ea45c35753b564d1adc175cbae6fe7
leeapril0612 <leeapril0612@openobject.net>
2022-02-18 13:40:20 +0900

    Add interface and getProfile Api to userApi

--- src/service/user/userAPI.ts
+++ src/service/user/userAPI.ts
@@ -1,6 +1,15 @@
 import { HttpRequest } from '@/util/http/httpRequest'
 
-export const login = (body: LoginBody) => {
+interface LoginReponse {
+  token: string
+}
+
+interface ReponseBody {
+  data: LoginReponse
+  code: number
+}
+
+export const login = (body: LoginBody): Promise<ReponseBody> => {
   return new HttpRequest().request({
     url: '/user/login',
     method: 'post',
@@ -8,5 +17,12 @@ export const login = (body: LoginBody) => {
     headers: {
       'Content-Type': 'application/json;charset=utf-8'
     }
+  })
+}
+
+export const getProfile = (): Promise<ReponseBody> => {
+  return new HttpRequest().request({
+    url: '/user/profile',
+    method: 'get'
   })
 }
