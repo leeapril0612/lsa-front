76cedb9647b911c4f589d30469de5000c6967b51
leeapril0612 <leeapril0612@openobject.net>
2022-02-21 17:21:10 +0900

    Add board listview

--- src/views/board/BoardList.vue
+++ src/views/board/BoardList.vue
@@ -2,48 +2,41 @@
   <div class="wrap">
     <div class="box">
       <div class="table-tool">
-          <button class="btn" @click="btn">글쓰기</button>
+          <button class="btn" @click="boardCreate">글쓰기</button>
       </div>
       <div class="glass">
-        <table cellpadding="0" cellspacing="0" border="0">
-          <thead>
-            <tr>
-              <th class="idx">#</th>
-              <th>Title</th>
-              <th>Content</th>
-              <th class="username">Username</th>
-              <th class="date">Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in [1,2,3,4,5,6]" v-bind:key="item">
-              <td class="idx">{{item}}</td>
-              <td>Lorem ipsum dolor...</td>
-              <td >consectetur adipisicing elit...</td>
-              <td class="username">admin</td>
-              <td class="date">2022-02-17</td>
-            </tr>
-          </tbody>
-        </table>
+        <list-view
+          :boards="this.$store.state.Board.boards"
+          :isLoading="this.$store.state.Board.isLoadingBoards"
+        />
       </div>
     </div>
   </div>
 </template>
 
-<script>
-import { getBoards } from '@/service/board/boardAPI'
+<script lang="ts">
 import Vue from 'vue'
+import ListView from './component/ListView.vue'
 
 export default Vue.extend({
-  name: 'board',
-  mounted () {
-    getBoards().then((res) => {
-      console.log(res.data)
-    })
+  components: { ListView },
+  created () {
+    this.init()
+  },
+  beforeDestroy () {
+    this.$store.commit('setBoards', [])
   },
   methods: {
-    btn () {
-      alert('sada')
+    init () {
+      this.$store.dispatch('GET_BOARDS')
+    },
+    boardCreate () {
+      this.$router.push('/board/create')
+    }
+  },
+  computed: {
+    list () {
+      return this.$store.state.Board.boards
     }
   }
 })
@@ -56,13 +49,6 @@ export default Vue.extend({
   .box {
     width: 60%;
   }
-  .btn {
-    color: #fff;
-    background: none;
-    padding: 5px 18px;
-    border-radius: 15px;
-    border: 1px solid #fff;
-  }
   .glass {
     border-radius: 10px;
     backdrop-filter: blur(10px);
@@ -73,28 +59,6 @@ export default Vue.extend({
     padding: 10px;
     display: flex;
     justify-content: right;
-  }
-  table {
-    width: 100%;
-    table-layout: fixed;
-  }
-  table th{
-    background-color: rgba(255,255,255,0.3);
-  }
-  table .idx{
-    width: 5%;
-  }
-  .date,.username {
-    width: 12%;
-  }
-  table th, table td {
-    padding: 20px 15px;
-    text-align: left;
-    font-weight: 500;
-    font-size: 12px;
-    color: #fff;
-    text-transform: uppercase;
-    border-bottom: solid 1px rgba(255,255,255,0.1);
   }
 
 </style>
---
+++ src/views/board/component/ListView.vue
@@ -0,0 +1,87 @@
+<template>
+  <table cellpadding="0" cellspacing="0" border="0">
+    <thead>
+      <tr>
+        <th class="idx">#</th>
+        <th>Title</th>
+        <th>Content</th>
+        <th class="username">Username</th>
+        <th class="date">Date</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in boards" v-bind:key="item.idx">
+        <td class="idx">{{item.id}}</td>
+        <td><router-link :to="{
+          name: 'boardRead',
+          params: {
+            id: item.id
+          }
+        }">{{item.title}}</router-link></td>
+        <td >{{item.content}}</td>
+        <td class="username">{{item.username}}</td>
+        <td class="date">{{getDay(item.createdDate)}}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts">
+import dayjs from 'dayjs'
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'ListView',
+  props: {
+    boards: {
+      type: Array,
+      required: true
+    },
+    isLoading: {
+      type: Boolean,
+      required: true
+    }
+  },
+  created () {
+    console.log(this.boards)
+  },
+  methods: {
+    getDay (time: number) {
+      return dayjs(time).format('YYYY-MM-DD')
+    }
+  }
+
+})
+</script>
+
+<style>
+ table {
+    width: 100%;
+    table-layout: fixed;
+  }
+  table th{
+    background-color: rgba(255,255,255,0.3);
+    text-transform: uppercase;
+  }
+  table .idx{
+    width: 5%;
+  }
+  .date,.username {
+    width: 12%;
+  }
+  table th, table td {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    padding: 20px 15px;
+    text-align: left;
+    font-weight: 500;
+    font-size: 12px;
+    color: #fff;
+    border-bottom: solid 1px rgba(255,255,255,0.1);
+  }
+  table a {
+    color: #fff;
+    text-decoration: none;
+  }
+</style>
