f5f370f14ac68670a1df055ebc0f08b702b956bd
leeapril0612 <leeapril0612@openobject.net>
2022-02-21 17:03:38 +0900

    Add global style

--- src/App.vue
+++ src/App.vue
@@ -52,4 +52,29 @@ export default {
     padding-top: 75px;
     background: rgba(0,0,0,0.25)
   }
+
+  .glass {
+    border-radius: 10px;
+    backdrop-filter: blur(10px);
+    border: 2px solid rgba(255,255,255,0.1);
+    box-shadow: 0 0 100px rgb(8 7 16 / 30%);
+    padding: 15px;
+  }
+  .btn {
+    cursor: pointer;
+    color: #fff;
+    background: none;
+    padding: 5px 18px;
+    border-radius: 15px;
+    border: 1px solid #fff;
+    position: relative;
+    overflow: hidden;
+    transition: 0.15s;
+    z-index: 2;
+  }
+  .btn:hover{
+    color: #000;
+    background: #fff;
+    border: 1px solid linear-gradient(45deg, #FC466B, #3F5EFB);;
+  }
 </style>
